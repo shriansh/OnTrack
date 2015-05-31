4e2d1c84aa8ef6faa3781153222b5814983873b9
Shriansh Shrivastava <shriansh@gmail.com>
2015-05-30 23:20:52 -0500

    personal boards fixed

--- app/controllers/personal_boards_controller.rb
+++ app/controllers/personal_boards_controller.rb
@@ -17,29 +17,32 @@ class PersonalBoardsController < ApplicationController
     @personal_board = PersonalBoard.new
   end
 
- def new_my_board
+  def new_my_board
     @personal_board = PersonalBoard.new
+    @users = User.all
   end
 
 
 
   def create
     @personal_board = PersonalBoard.new
-    @personal_board.boardmember5_id = params[:boardmember5]
-    @personal_board.boardmember4_id = params[:boardmember4]
-    @personal_board.boardmember3_id = params[:boardmember3]
-    @personal_board.boardmember2_id = params[:boardmember2]
-    @personal_board.boardmember1_id = params[:boardmember1]
-
-    @personal_board.boardmember5 = User.find(params[:boardmember5].to_i).username
-    @personal_board.boardmember4 = User.find(params[:boardmember4].to_i).username
-    @personal_board.boardmember3 = User.find(params[:boardmember3]).username
-    @personal_board.boardmember2 = User.find(params[:boardmember2]).username
-    @personal_board.boardmember1 = User.find(params[:boardmember1]).username
-
     @personal_board.user = current_user.username
     @personal_board.user_id=current_user.id
-    @personal_board.user_id = current_user.id
+
+    @personal_board.boardmember5_id = params[:boardmember5_id]
+    @personal_board.boardmember4_id = params[:boardmember4_id]
+    @personal_board.boardmember3_id = params[:boardmember3_id]
+    @personal_board.boardmember2_id = params[:boardmember2_id]
+    @personal_board.boardmember1_id = params[:boardmember1_id]
+
+    @personal_board.boardmember5 = User.find(params[:boardmember5_id]).username
+    @personal_board.boardmember4 = User.find(params[:boardmember4_id]).username
+    @personal_board.boardmember3 = User.find(params[:boardmember3_id]).username
+    @personal_board.boardmember2 = User.find(params[:boardmember2_id]).username
+    @personal_board.boardmember1 = User.find(params[:boardmember1_id]).username
+
+
+
 
     if @personal_board.save
       redirect_to "/personal_boards", :notice => "Personal board created successfully."
@@ -49,11 +52,11 @@ class PersonalBoardsController < ApplicationController
   end
 
 
-def edit_my_board
+  def edit_my_board
 
-  @personal_board = PersonalBoard.where(user_id: current_user.id).first
+    @personal_board = PersonalBoard.where(user_id: current_user.id).first
 
-end
+  end
 
 
 
@@ -63,13 +66,20 @@ end
 
   def update
     @personal_board = PersonalBoard.find(params[:id])
+        @personal_board.user = current_user.username
+    @personal_board.user_id=current_user.id
 
-    @personal_board.boardmember5 = params[:boardmember5]
-    @personal_board.boardmember4 = params[:boardmember4]
-    @personal_board.boardmember3 = params[:boardmember3]
-    @personal_board.boardmember2 = params[:boardmember2]
-    @personal_board.boardmember1 = params[:boardmember1]
-    @personal_board.user = params[:user]
+    @personal_board.boardmember5_id = params[:boardmember5_id]
+    @personal_board.boardmember4_id = params[:boardmember4_id]
+    @personal_board.boardmember3_id = params[:boardmember3_id]
+    @personal_board.boardmember2_id = params[:boardmember2_id]
+    @personal_board.boardmember1_id = params[:boardmember1_id]
+
+    @personal_board.boardmember5 = User.find(params[:boardmember5_id]).username
+    @personal_board.boardmember4 = User.find(params[:boardmember4_id]).username
+    @personal_board.boardmember3 = User.find(params[:boardmember3_id]).username
+    @personal_board.boardmember2 = User.find(params[:boardmember2_id]).username
+    @personal_board.boardmember1 = User.find(params[:boardmember1_id]).username
 
     if @personal_board.save
       redirect_to "/personal_boards", :notice => "Personal board updated successfully."
